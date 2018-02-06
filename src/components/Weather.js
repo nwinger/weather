397bb8679a5d3538d31d87e77a8a50abe6c651c6
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-02-06 21:25:35 +0100

    Add style and layout

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,28 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Weather = props => (
-  <div>
-    {props.city &&
-      props.country && (
-        <p>
-          Location: {props.city}, {props.country}
-        </p>
-      )}
-    {props.temperature && <p>Temperature: {props.temperature}</p>}
-    {props.humidity && <p>Humidity: {props.humidity}</p>}
-    {props.description && <p>Condidtions: {props.description}</p>}
-    {props.error && <p>{props.error}</p>}
-  </div>
+	<div className="weather__info">
+		{props.city &&
+			props.country && (
+				<p className="weather__key">
+					Location:
+					<span className="weather__value">
+						{' '}
+						{props.city}, {props.country}
+					</span>
+				</p>
+			)}
+		{props.temperature && (
+			<p className="weather__key">
+				Temperature:
+				<span className="weather__value"> {props.temperature}</span>
+			</p>
+		)}
+		{props.humidity && (
+			<p className="weather__key">
+				Humidity:
+				<span className="weather__value"> {props.humidity}</span>
+			</p>
+		)}
+		{props.description && (
+			<p className="weather__key">
+				Condidtions:
+				<span className="weather__value"> {props.description}</span>
+			</p>
+		)}
+		{props.error && <p className="weather__error">{props.error}</p>}
+	</div>
 );
 // TODO types
 // TODO default props
 Weather.propTypes = {
-  city: '',
-  country: '',
-  temperature: '',
-  humidity: '',
-  description: '',
-  error: ''
+	city: '',
+	country: '',
+	temperature: '',
+	humidity: '',
+	description: '',
+	error: ''
 };
 
 export default Weather;
